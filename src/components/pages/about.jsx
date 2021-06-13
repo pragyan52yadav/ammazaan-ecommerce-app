891fe99696a55c05ddfad3a9eadda13dd0ee8a2a
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-06-13 20:48:13 +0530

    fixed about

--- src/components/pages/about.jsx
+++ src/components/pages/about.jsx
@@ -3,13 +3,15 @@ import { Nav, Footer } from "../layout";
 import DevProfile from "../profile/dev_profile";
 
 const About = () => {
-  return <>
-    <Nav />
-    <DevProfile />
-    <DevProfile />
-    <DevProfile />
-    <Footer />
-  </>;
+  return (
+    <>
+      <Nav />
+      <DevProfile />
+      <DevProfile />
+      <DevProfile />
+      <Footer />
+    </>
+  );
 };
 
 export default About;
