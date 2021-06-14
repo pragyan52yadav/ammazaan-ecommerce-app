07002b6b2937a4ed244bc01e391963757af63c20
Khwaish <khwaish.rupani@gmail.com>
2021-06-14 21:45:29 +0530

    Cart styled

--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,17 +1,19 @@
 import "../../styles/cart.css";
 
 const Cart = (props) => {
-    console.log("hehehe", props.cart);
     return (
-        <div>
-            <h3>{props.item.title}</h3>
-            <img src={props.item.image} alt="" />
-            <button
-                className="cross"
-                onClick={() => props.removeItem(props.item)}
-            >
-                x
-            </button>
+        <div className="cards">
+            <div className="product-card">
+                <h3>{props.item.title}</h3>
+                <img className="product_image" src={props.item.image} alt="" />
+                <h3 className="price">{props.item.price}</h3>
+                <button
+                    className="cross"
+                    onClick={() => props.removeItem(props.item)}
+                >
+                    x
+                </button>
+            </div>
         </div>
     );
 };
