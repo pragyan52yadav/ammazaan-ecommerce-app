4ed4ad8d12615909b129b0ed6234a8fe747323ec
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-06-14 21:44:29 +0530

    about styled

--- src/components/profile/dev_profile.jsx
+++ src/components/profile/dev_profile.jsx
@@ -1,13 +1,24 @@
 import "../../styles/dev_profile.css";
+import { FaInstagram } from "react-icons/fa";
 
 const DevProfile = (props) => {
   return (
-    <div className="profile">
-      <div className="img_div">
-        <img src={props.images.image} alt="" className="dev_image" />
-      </div>
-      <div className="name">
-        <p>{props.images.name}</p>
+    <div className="profile_div">
+      <div className="profile">
+        <div className="img_div">
+          <img src={props.images.image} alt="" className="dev_image" />
+        </div>
+        <div className="name">
+          <p>{props.images.name}</p>
+        </div>
+        <div className="comment">
+          <span className="comment_txt">{props.images.comment}</span>
+        </div>
+        <div className="insta-icon">
+          <a href={props.images.link} className="insta-link" target="_blank" rel="noreferrer">
+            <FaInstagram className="insta" />
+          </a>
+        </div>
       </div>
     </div>
   );
