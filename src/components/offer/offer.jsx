af4d6450db10713ac8f17d61267ef6ad4b2e859a
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-06-14 06:25:52 +0530

    offer working, styling will be done after rest of the styling

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -1,11 +1,23 @@
-import "../../styles/offer.css";
+import Slider from './slider';
+import '../../styles/offer.css'
+
+const ImageData = [
+  {
+    image: "https://images.freeimages.com/images/small-previews/eee/summer-nature-3-1370238.jpg",
+  },
+  {
+    image: "https://images.freeimages.com/images/small-previews/773/koldalen-4-1384902.jpg",
+  },
+  {
+    image: "https://images.freeimages.com/images/small-previews/716/flower-1372780.jpg",
+  },
+  {
+    image: "https://images.freeimages.com/images/small-previews/10f/autumn-1-1382513.jpg",
+  },
+];
 
 const Offer = () => {
-  return (
-    <>
-      <div>Something will come</div>
-    </>
-  );
+  return <Slider slides={ImageData} />;
 };
 
 export default Offer;
---
+++ src/components/offer/slideImage.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SlideImage = styled.img`
+  width: 400px;
+  height: 300px;
+  object-fit: cover;
+`;
+
+export const StyledSlider = styled.div`
+  postiton: relative;
+  display: flex;
+  justify-center: center;
+  align=items: center;
+`;
