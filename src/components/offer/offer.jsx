8c68778b586dc2537e29367f61a682e55db44077
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-06-14 13:46:32 +0530

    offer changed

--- src/components/offer/offer.jsx
+++ src/components/offer/offer.jsx
@@ -2,8 +2,8 @@ import Item1 from "../assets/Item1.png";
 import Item2 from "../assets/Item2.png";
 import Item3 from "../assets/Item3.png";
 import Item4 from "../assets/Item4.png";
-import Slider from './slider';
-import '../../styles/offer.css'
+import Slider from "./slider";
+import "../../styles/offer.css";
 
 const ImageData = [
   {
@@ -21,7 +21,13 @@ const ImageData = [
 ];
 
 const Offer = () => {
-  return <Slider slides={ImageData} />;
+  return (
+    <>
+      <div className="Offer_div">
+        <Slider slides={ImageData} />
+      </div>
+    </>
+  );
 };
 
 export default Offer;
